ee8394b8fb1aa9739d81e575b09c457b29c10475
Feras Kahwaji <feraskahwaji@hotmail.com>
2023-03-01 23:17:39 -0500

    Updates quiz question info. A work in progress.

--- frontend/src/components/quiz/questions.js
+++ frontend/src/components/quiz/questions.js
@@ -1,16 +1,59 @@
 export const catQuestions = [
-    { title: 'Size', description: `` },
-    { title: 'Size', description: `` },
-    { title: 'Shedding', description: `How much can you tolerate shedding? You'll need to deal with some level of hair on your clothes and in your house. However, shedding varies depending on the breed. If you like to play with balls of fluff, then max out that meter!` },
-    { title: 'Playfulness', description: `Are you looking for a fun and playful cat to add to your family? Some cats are always ready to play with their mouse-toy, while others are more relaxed and laid-back. Think about how much time you have to play with your cat each day, and if you have other family members or other pets who can join in the fun.` },
-    { title: 'Vocalization', description: `How "talkative" would you like your cat to be? Do you prefer your cat to be vocal when it wants attention? Or do you prefer the silent type?` },
-    { title: 'Affection', description: `Some cats bond closely to one person and are indifferent towards everyone else, and some others shower the family with love affection. How affectionate would you like your cat to be?` },
+    {
+        title: 'Size', description: `Choose the size of your cat.`,
+        options: [{ image: "/Mocha.PNG", text: "Small", alt: "Small cat" }, { image: "/Mocha.PNG", text: "Medium", alt: "Medium cat" }, { image: "/Mocha.PNG", text: "Large", alt: "Large cat" }, { image: "/question-mark-cat.PNG", text: "Open to any size", alt: "Open to any size cat", style: true }],
+        optionsType: "slider"
+    },
+    {
+        title: 'Size', description: `Choose the size of your cat.`,
+        options: [{ image: "/Mocha.PNG", text: "Small", alt: "Small cat" }, { image: "/Mocha.PNG", text: "Medium", alt: "Medium cat" }, { image: "/Mocha.PNG", text: "Large", alt: "Large cat" }, { image: "/question-mark-cat.PNG", text: "Open to any size", alt: "Open to any size cat", style: true }]
+    },
+    {
+        title: 'Grooming', description: `How much time would you like to spend grooming your cat? Fluffy cats require more grooming`,
+        options: [{ image: "/grooming-0.png", text: "Never", alt: "Never or barely groom the cat" }, { image: "/grooming-1.png", text: "Once per week", alt: "Groom the cat once per week" }, { image: "/grooming-2.png", text: "Two to three times per week", alt: "Groom the cat twice to three times per week" }, { image: "/grooming-everyday.png", text: "Everyday", alt: "Groom the cat every day" }],
+        optionsType: "slider"
+    },
+    {
+        title: 'Shedding',
+        description: `How much can you tolerate shedding? You'll need to deal with some level of hair on your clothes and in your house. However, shedding varies depending on the breed. If you like to play with balls of fluff, then max out that meter!`,
+        images: ["/Mocha.PNG", "/Mocha.PNG"]
+    },
+    {
+        title: 'Playfulness',
+        description: `Are you looking for a fun and playful cat to add to your family? Some cats are always ready to play with their mouse-toy, while others are more relaxed and laid-back. Think about how much time you have to play with your cat each day, and if you have other family members or other pets who can join in the fun.`,
+        images: ["/Mocha.PNG", "/Mocha.PNG"]
+    },
+    {
+        title: 'Vocalization',
+        description: `How "talkative" would you like your cat to be? Do you prefer your cat to be vocal when it wants attention? Or do you prefer the silent type?`,
+        images: ["/Mocha.PNG", "/Mocha.PNG"]
+    },
+    {
+        title: 'Affection',
+        description: `Some cats bond closely to one person and are indifferent towards everyone else, and some others shower the family with love affection. How affectionate would you like your cat to be?`,
+        images: ["/Mocha.PNG", "/Mocha.PNG"]
+    },
 ];
 
 export const dogQuestions = [
-    { title: 'Size', description: `` },
-    { title: 'Shedding', description: `How much can you tolerate shedding? You'll need to deal with some level of hair on your clothes and in your house. However, shedding varies depending on the breed. If you like to play with balls of fluff, then max out that meter!` },
-    { title: 'Playfulness', description: `Are you looking for a fun and playful pup to add to your family? Some dogs are always ready for a game of fetch or tag, while others are more relaxed and laid-back. Think about how much time you have to play with your pup each day, and if you have other family members or other pets who can join in the fun.` },
-    { title: 'Vocalization', description: `How "talkative" would you like your dog to be? Do you prefer your dog to be vocal when it wants attention? Or do you prefer the silent type?` },
-    { title: 'Affection', description: `Some dogs bond closely to one person and are indifferent towards everyone else, and some others shower the family with love affection. How affectionate would you like your dog to be?` },
+    {
+        title: 'Size',
+        description: ``
+    },
+    {
+        title: 'Shedding',
+        description: `How much can you tolerate shedding? You'll need to deal with some level of hair on your clothes and in your house. However, shedding varies depending on the breed. If you like to play with balls of fluff, then max out that meter!`
+    },
+    {
+        title: 'Playfulness',
+        description: `Are you looking for a fun and playful pup to add to your family? Some dogs are always ready for a game of fetch or tag, while others are more relaxed and laid-back. Think about how much time you have to play with your pup each day, and if you have other family members or other pets who can join in the fun.`
+    },
+    {
+        title: 'Vocalization',
+        description: `How "talkative" would you like your dog to be? Do you prefer your dog to be vocal when it wants attention? Or do you prefer the silent type?`
+    },
+    {
+        title: 'Affection',
+        description: `Some dogs bond closely to one person and are indifferent towards everyone else, and some others shower the family with love affection. How affectionate would you like your dog to be?`
+    },
 ];
--- frontend/src/components/quiz/quiz.js
+++ frontend/src/components/quiz/quiz.js
@@ -1,4 +1,4 @@
-import React, { useState, useLayoutEffect } from "react";
+import React, { useState, /*useLayoutEffect*/ } from "react";
 import { useParams } from "react-router-dom";
 import { catQuestions } from "./questions";
 import { dogQuestions } from "./questions";
@@ -6,9 +6,9 @@ import './quiz.css'
 
 function Quiz() {
     // for some reason react displays the page in the middle of the screen
-    useLayoutEffect(() => {
-        window.scrollTo(0, 0)
-    });
+    // useLayoutEffect(() => {
+    //     window.scrollTo(0, 0)
+    // });
     //quiz animal styling
     const dogStyling = useParams().animal === "dog" ? true : false;
 
@@ -19,8 +19,8 @@ function Quiz() {
     //quiz buttons
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [backBtnVisible, setbackBtnVisible] = useState(false);
-
-
+    //range option value
+    const [optionValue, setOptionValue] = useState(0);
     const handleNextClick = () => {
 
         const nextQuestion = currentQuestion + 1;
@@ -54,14 +54,88 @@ function Quiz() {
         }
     };
 
+    const handleOptionClick = (e) => {
+        // console.log(e.currentTarget.previousSibling)
+        const allElements = document.querySelectorAll('*');
+        allElements.forEach((element) => {
+            element.classList.remove('cat-title');
+        });
+        e.currentTarget.previousSibling.classList.add('cat-title');
+    };
+
+    const handleOptionChange = (e, optionText) => {
+        let frontCard = document.getElementById("front");
+        let backCard = document.getElementById("back");
+        frontCard.classList.add("front");
+        //backCard.classList.add("back");
+        //backCard.style.display = "";
+
+
+
+        setTimeout(() => {
+            frontCard.classList.remove("front");
+            //backCard.classList.remove("back");
+            //backCard.style.display = "none";
+
+        }, 700);
+        setOptionValue(e.target.value);
+    };
+
+    //maybe a measure tape with a cat standing on two feet for "sizes"
+    var optionsArray = [];
+    // create and style the options
+    if (questions[currentQuestion].optionsType === "slider") {
+        console.log("current:", optionValue)
+        let sliderLength = questions[currentQuestion].options.length - 1;
+        let option = questions[currentQuestion].options[optionValue];
+        let nextOption = option;
+        if (parseInt(optionValue) + 1 < sliderLength) {
+            nextOption = questions[currentQuestion].options[parseInt(optionValue) + 1];
+        } else if (parseInt(optionValue) - 1 < sliderLength) {
+            nextOption = questions[currentQuestion].options[parseInt(optionValue) - 1];
+        }
+
+        optionsArray =
+            (
+                <div className="col mt-4 spacing">
+                    <div className="col-card">
+                        <h4 className="form-label mb-5">{option.text}</h4>
+                        <div className="card-side" id="front">
+                            <img id={option.text} className={`range-image ${dogStyling ? "dog-range-img" : "cat-range-img"}`} src={option.image} alt={option.alt} />
+                        </div>
+                        {/* <div className="card-side" id="back">
+                            <img id={nextOption.text} className={`range-image ${dogStyling ? "dog-range-img" : "cat-range-img"}`} src={nextOption.image} alt={nextOption.alt} />
+                        </div> */}
+                    </div>
+                    <br></br>
+                    <input type="range" className="custom-range mt-5" value={optionValue} min="0" max={sliderLength} id="customRange" onChange={e => handleOptionChange(e, option.text)}></input>
+                </div>
+            );
+
+    } else {
+        optionsArray = questions[currentQuestion].options.map((option) => {
+            return (
+                <div className="col mt-4" key={option.text}>
+                    <h4 className={`mb-3`}>{option.text}</h4>
+                    <button className={`question-img ${option.style ? "button-color" : ""}`} onClick={e => handleOptionClick(e)}><img src={option.image} alt={option.alt} /></button>
+                </div>
+
+            );
+        });
+    }
+
+
     return (
         <>
             <div className="page container" style={{ marginTop: "250px" }}>
                 <div className={`${dogStyling ? "dog-style" : "cat-style"} card d-flex my-auto`}>
-                    <div className="card-body mt-5">
+                    <div className="card-body mt-3">
                         <span>Question {currentQuestion + 1}/{questions.length}</span>
                         <h5 className="card-title">{questions[currentQuestion].title}</h5>
                         <p className="card-text">{questions[currentQuestion].description}</p>
+                        <div className="row">{optionsArray}</div>
+                        <br></br>
+                        <br></br>
                         <div className="row">
                             {backBtnVisible &&
                                 <div className="col">
